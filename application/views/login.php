f38721a86485844d157d6362bae370b77ab21f76
Ritik Katiyar <ritik.katiyar@eninrac.com>
2024-07-17 17:52:55 +0530

    login-complete

--- application/views/login.php
+++ application/views/login.php
@@ -1,11 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="<?= base_url('assets/fonts/fonts.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>" />
 </head>
+
 <body>
-    ritik
+
+    <section>
+        <div class="login-header">
+            <a href="">
+                <img src="<?= base_url() ?>assets/images/logo_dark.png" style="max-width: 200px;" alt="LOGO" class="footer-header">
+            </a>
+        </div>
+    </section>
+
+    <section class="form-section">
+        <div class="form-head text-center">
+            <h1 style="">Sign in to your account</h1>
+            <div class="d-flex flex gap-2">
+                <p>Don't have an account?</p>
+                <p>Create your free account</p>
+                
+
+            </div>
+
+        </div>
+        <form action="" method="POST">
+            <input type="hidden" name="_token">
+            <div class="mb-2">
+                <label for="username" class=" ">Email Address</label>
+                <input data-testid="Email-Address-input" id="username" name="log" type="text" autocomplete="username" placeholder="Email Address" required class="form-control bg-white py-3">
+            </div>
+            <div class="mb-2">
+                <label for="password" class=" ">Password</label>
+                <input data-testid="password-input" id="password" name="pwd" type="password" autocomplete="current-password" placeholder="Password" required class="form-control py-3">
+            </div>
+            <div class="d-flex justify-content-end mt-3 mb-4">
+
+                <div class="text-sm">
+                    <a href="/recover-password" class="text-grey font-medium">Forgot password?</a>
+                </div>
+            </div>
+            <div>
+                <button type="submit" data-testid="login-button" class="btn btn-primary text-white py-2 px-4 rounded-md " style="width: 100%;">Login</button>
+            </div>
+        </form>
+    </section>
 </body>
+
 </html>
