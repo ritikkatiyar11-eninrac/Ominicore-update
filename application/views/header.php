3b9ef3934378fba0662d5662806cfa9e9e484f8a
mahesheninrac <mahesh.solanke@eninrac.com>
2024-07-29 10:17:26 +0530

    updated header and done with responsive design

--- application/views/header.php
+++ application/views/header.php
@@ -10,12 +10,62 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
     <style>
-        .header_list li .header-link{
+        .header_list li .header-link {
             color: #0f3f6e;
         }
 
-        .header_list li a:hover{
+        .header_list li a:hover {
             color: #3243e9;
+        }
+
+        .header-right {
+            background-image: -webkit-gradient(linear, left top, right top, from(transparent), to(#fff)),
+                -webkit-gradient(linear, left top, left bottom, from(transparent), to(#fff)),
+                -webkit-gradient(linear, left top, left bottom, from(#eee9ff), to(#eee9ff));
+            height: 80px;
+            margin-bottom: 0;
+            width: 70%;
+
+        }
+
+        @media(max-width:500px){
+            .header-right {
+                width: 60%;
+            }
+        }
+
+        .header-right .header_list {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            list-style: none;
+        }
+
+        .header-right .header-iconlist {
+            display: none;
+            justify-content: flex-end;
+            align-items: center;
+            list-style: none;
+        }
+
+        @media (max-width:870px) {
+            .header-right .header_list {
+                display: none;
+            }
+
+            .header-right .header-iconlist {
+                display: flex;
+                gap: 20px;
+
+            }
+        }
+
+        @media(min-width:870px) {
+            .header-right {
+                width: calc(100% - 370px);
+            }
+
+
         }
     </style>
 </head>
@@ -33,16 +83,28 @@
                     </div>
 
                 </div>
-                <ul style="margin-bottom: 0; padding-left:0;" class="d-flex flex-wrap gap-1 list-style-none"></ul>
-                <div class="" style="height:80px; width:calc(100% - 370px);margin-bottom: 0; background-image: -webkit-gradient(linear, left top, right top, from(transparent), to(#fff)), -webkit-gradient(linear, left top, left bottom, from(transparent), to(#fff)), -webkit-gradient(linear, left top, left bottom, from(#eee9ff), to(#eee9ff));">
-                    <ul class="header_list d-flex list-style-none justify-content-end align-items-center" style="height: 100%;">
+
+                <div class="header-right">
+                    <ul style="margin-bottom: 0; padding-left:0; height:100%;" class="header-iconlist">
+                    <li class="d-flex justify-content-center align-items-center py-2 px-3  cursor-pointer">
+                            <a href="">
+                            <i class="icofont-navigation-menu"></i>
+                            </a>
+                        </li>
+                        <li class="d-flex justify-content-center align-items-center py-2 px-3  cursor-pointer">
+                            <a href="">
+                                <i class="icofont-search-1"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="header_list " style="height: 100%;">
                         <li class="NewsLetter  d-flex justify-content-center align-items-center  py-2 px-3  cursor-pointer">
                             <a href="<?= base_url('infographicpage') ?>" class="header-link" target="_blank" rel="noopener noreferrer">Infographics</a>
                         </li>
                         <li class="subscriptions d-flex justify-content-center align-items-center py-2 px-3  cursor-pointer" style=" border-radius:6px">
                             <a href="<?= base_url('subscribe') ?>" class="header-link" style="line-height:20px" target="_blank" rel="noopener noreferrer">Subscriptions</a>
                         </li>
-                        <li class="NewsLetter  d-flex justify-content-center align-items-center  py-2 px-3  cursor-pointer" >
+                        <li class="NewsLetter  d-flex justify-content-center align-items-center  py-2 px-3  cursor-pointer">
                             <a href="<?= base_url('newsletter') ?>" class="header-link" target="_blank" rel="noopener noreferrer">NewsLetter</a>
                         </li>
                         <li class="NewsLetter  d-flex justify-content-center align-items-center  py-2 px-3  cursor-pointer">
