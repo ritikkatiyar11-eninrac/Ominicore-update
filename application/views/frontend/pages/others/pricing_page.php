4bb4778aaaf066c3398a8f40c065eb598a218413
mahesheninrac <mahesh.solanke@eninrac.com>
2024-08-27 18:41:12 +0530

    added flex-grow for container

--- application/views/frontend/pages/others/pricing_page.php
+++ application/views/frontend/pages/others/pricing_page.php
@@ -1,3 +1,49 @@
 <?php require(APPPATH . 'views/frontend/components/header.php'); ?>
 
+<div class="container-fluid">
+    <section class="pricing-page d-flex w-100">
+        <div class="flex-grow-1">
+            <div class="price-content border py-4 px-2">
+                <div class="">
+                    <div class="fs-22 fw-400 mb-1 text-center">Bussiness Starter</div>
+                </div>
+                <div class=""></div>
+            </div>
+        </div>
+        <div class="flex-grow-1">
+            <div class="price-content border py-4 px-2">
+                <div class="">
+                    <div class="fs-22 fw-400 mb-1 text-center">Bussiness Starter</div>
+                </div>
+                <div class=""></div>
+            </div>
+        </div>
+        <div class="flex-grow-1">
+            <div class="price-content border py-4 px-2">
+                <div class="">
+                    <div class="fs-22 fw-400 mb-1 text-center">Bussiness Starter</div>
+                </div>
+                <div class=""></div>
+            </div>
+        </div>
+        <div class="flex-grow-1">
+            <div class="price-content border py-4 px-2">
+                <div class="">
+                    <div class="fs-22 fw-400 mb-1 text-center">Bussiness Starter</div>
+                </div>
+                <div class=""></div>
+            </div>
+        </div>
+        <div class="flex-grow-1">
+            <div class="price-content border py-4 px-2">
+                <div class="">
+                    <div class="fs-22 fw-400 mb-1 text-center">Bussiness Starter</div>
+                </div>
+                <div class=""></div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
 <?php require(APPPATH . 'views/frontend/components/footer.php'); ?>
